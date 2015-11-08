f5d34670cd695c4b63627f9e859f21ba715df8f7
Anshul Jain <anshul.jain1207@gmail.com>
2015-11-08 04:09:56 -0700

    Update runOperations.sh
    Adding operations

--- Scripts/runOperations.sh
+++ Scripts/runOperations.sh
@@ -2,4 +2,9 @@
 cd /home/user22/Desktop/GeospatialOperations/GeospatialOperations/
 mvn package
 /usr/local/spark/bin/spark-submit --class "DDS.team22.GeospatialOperations.UnionPolygon" --master local[4] /home/user22/Desktop/GeospatialOperations/GeospatialOperations/target/GeospatialOperations-0.0.1-SNAPSHOT.jar
+/usr/local/spark/bin/spark-submit --class "DDS.team22.GeospatialOperations.ConvHull" --master local[4] /home/user22/Desktop/GeospatialOperations/GeospatialOperations/target/GeospatialOperations-0.0.1-SNAPSHOT.jar
+/usr/local/spark/bin/spark-submit --class "DDS.team22.GeospatialOperations.farthestPair" --master local[4] /home/user22/Desktop/GeospatialOperations/GeospatialOperations/target/GeospatialOperations-0.0.1-SNAPSHOT.jar
+/usr/local/spark/bin/spark-submit --class "DDS.team22.GeospatialOperations.ClosestPair" --master local[4] /home/user22/Desktop/GeospatialOperations/GeospatialOperations/target/GeospatialOperations-0.0.1-SNAPSHOT.jar
+/usr/local/spark/bin/spark-submit --class "DDS.team22.GeospatialOperations.SpatialJoinQuery" --master local[4] /home/user22/Desktop/GeospatialOperations/GeospatialOperations/target/GeospatialOperations-0.0.1-SNAPSHOT.jar
+/usr/local/spark/bin/spark-submit --class "DDS.team22.GeospatialOperations.RangeQuery" --master local[4] /home/user22/Desktop/GeospatialOperations/GeospatialOperations/target/GeospatialOperations-0.0.1-SNAPSHOT.jar
 cd ~/
